a367303ae9ef077dff17694cc3ebe53ef993cfad
om-dev007 <officialomnarayan@gmail.com>
2025-12-02 00:09:15 +0530

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>This website is only for my practice , here i have learnt about useParams and dynamic routing </h1>
+    </div>
   )
 }
 
