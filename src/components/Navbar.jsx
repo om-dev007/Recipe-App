7a735d4d40f6d2e9f4c6c4a8b38628b8a5e63dd9
om-dev007 <officialomnarayan@gmail.com>
2025-12-01 17:34:35 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <div className=' flex justify-between p-5 rounded-2xl '>
-      <div className='text-red-500'>Recipe App</div>
-      <div className="flex gap-4">
+      <div className='font-semibold text-2xl p-2'>RecipeApp</div>
+      <div className="flex gap-4 p-2">
         <NavLink className={({ isActive }) =>  ` ${isActive ? "text-red-700" : "text-black"} `} to='/' >Home</NavLink>
         <NavLink className={({ isActive }) => ` ${isActive ? "text-red-700" : "text-black"} `}
  to='/recipe' >Recipe</NavLink>
