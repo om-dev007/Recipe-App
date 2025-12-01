436ef015783849ae01be6523ed73e9f75c2579b6
om-dev007 <officialomnarayan@gmail.com>
2025-12-01 17:49:34 +0530

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='p-4'>
+        <div className='object-contain flex justify-center'>
+            <img className='h-auto rounded-2xl ' src='/public/hero-sec.jpg' alt="" />
+        </div>
+    </div>
   )
 }
 
