0e3dba549516a45b77e7f6ad073374bab52bcb52
om-dev007 <officialomnarayan@gmail.com>
2025-12-02 13:21:48 +0530

    theme context done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,13 @@ const Navbar = () => {
   return (
     <div className=' flex justify-between p-5 rounded-2xl '>
       <div className='font-semibold text-2xl p-2'>RecipeApp</div>
-      <div className="flex gap-4 p-2">
+      <div className="flex gap-4 items-center p-2">
         <NavLink className={({ isActive }) =>  ` ${isActive ? "text-red-700" : "text-black"} `} to='/' >Home</NavLink>
         <NavLink className={({ isActive }) => ` ${isActive ? "text-red-700" : "text-black"} `}
  to='/recipe' >Recipe</NavLink>
         <NavLink className={({ isActive }) => ` ${isActive ? "text-red-700" : "text-black"} `}
           to='/about' >About</NavLink>
+          <button className="bg-orange-600 px-2 py-2 cursor-pointer hover:scale-105 transition-all rounded text-white "> Change to theme </button>
       </div>
     </div>
   )
