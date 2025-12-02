f4f36bf2798a97c707e670a0d75847b78621bad5
om-dev007 <officialomnarayan@gmail.com>
2025-12-02 13:38:32 +0530

    theme context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom"
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeContext"
 
 const Navbar = () => {
+  const {theme, toggleTheme} = useContext(ThemeContext)
   return (
     <div className=' flex justify-between p-5 rounded-2xl '>
       <div className='font-semibold text-2xl p-2'>RecipeApp</div>
@@ -10,7 +13,7 @@ const Navbar = () => {
  to='/recipe' >Recipe</NavLink>
         <NavLink className={({ isActive }) => ` ${isActive ? "text-red-700" : "text-black"} `}
           to='/about' >About</NavLink>
-          <button className="bg-orange-600 px-2 py-2 cursor-pointer hover:scale-105 transition-all rounded text-white "> Change to theme </button>
+          <button onClick={toggleTheme} className="bg-orange-600 px-2 py-2 cursor-pointer hover:scale-105 transition-all rounded text-white outline-0 "> Change to {theme} </button>
       </div>
     </div>
   )
